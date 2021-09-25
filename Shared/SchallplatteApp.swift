1448fd6a42066854d8543ab322b02cc77ee14203
Adrian <hannsadrian@gmail.com>
2021-09-25 15:22:43 +0200

    Initial Commit

---
+++ Shared/SchallplatteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchallplatteApp.swift
+//  Shared
+//
+//  Created by Adrian Böhme on 25.09.21.
+//
+
+import SwiftUI
+
+@main
+struct SchallplatteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
